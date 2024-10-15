1c8303abe029a3a8dca3ddc686774e9f1523606a
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-10-15 12:49:26 +0500

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import i18n from "../i18n";
 import { useTranslation } from "react-i18next";
+import { useContext } from "react";
+import { MoodeContext } from "../pages/Home";
+import { GiMoonOrbit } from "react-icons/gi";
+import { GiUbisoftSun } from "react-icons/gi";
+import { PiGitlabLogoFill } from "react-icons/pi";
 
 function Header() {
+  const { mood, setMood } = useContext(MoodeContext);
+
   const { t, i18n } = useTranslation();
 
   const [lang, setLange] = useState("uz");
@@ -10,50 +17,63 @@ function Header() {
   function handleChange(e) {
     setLange(e.target.value);
   }
-
   useEffect(() => {
     i18n.changeLanguage(lang);
   }, [lang]);
 
+  function handlMod(e) {
+    setMood(!mood);
+  }
+
   return (
-    <div className="drop-shadow-xl  bg-white">
-      <div className="container w-[1430px] m-auto  items-center p-5 ">
-        <div className="flex justify-between">
-          <div className="font-serif text-3xl font-bold">
-            <h2>YOUR LOGO</h2>
-          </div>
-          <nav className="flex gap-8 font-sans text-lg cursor-pointer ">
-            <p>{t("assets")}</p>
-            <p>{t('Creators')}</p>
-            <p>{t('Careers')}</p>
-            <p>{t('Go Pro')}</p>
-          </nav>
-          <div className="flex items-center gap-20">
-            <form class="max-w-sm mx-auto">
-              <label for="underline_select" class="sr-only">
-                Underline select
-              </label>
-              <select onChange={handleChange} value={lang}
-                id="underline_select"
-                class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+    <div className={mood ? "dark" : ""}>
+      <div className="drop-shadow-xl  bg-white dark:bg-black">
+        <div className="container w-[1430px] m-auto  items-center p-5 ">
+          <div className="flex justify-between">
+            <div className="font-serif text-3xl font-bold">
+              <h2 className="dark:text-white flex items-center gap-4 font-thin">
+                <div className="text-5xl">
+                  <PiGitlabLogoFill />
+                </div>
+                199.nzm
+              </h2>
+            </div>
+            <nav className="flex gap-8 font-sans text-lg cursor-pointer  dark:text-white">
+              <p>{t("assets")}</p>
+              <p>{t("Creators")}</p>
+              <p>{t("Careers")}</p>
+              <p>{t("Go Pro")}</p>
+            </nav>
+            <div className="flex items-center gap-20">
+              <form className="max-w-sm mx-auto">
+                <label htmlFor="underline_select" className="sr-only">
+                  Underline select
+                </label>
+                <select
+                  onChange={handleChange}
+                  value={lang}
+                  id="underline_select"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                >
+                  <option value="uz">Uzbbekistan</option>
+                  <option value="ru">Russiya</option>
+                  <option value="en">England</option>
+                </select>
+              </form>
+              <button
+                onClick={handlMod}
+                className=" text-blak pl-6 pr-6 rounded-xl mt-2"
               >
-                <option  selected>
-                  Choose a Launge
-                </option>
-                <option value="uz">Uzbbekistan</option>
-                <option value="ru">Russiya</option>
-                <option value="en">England</option>
-              </select>
-            </form>
-            <div>
-              <label class="relative inline-block h-7 w-[48px] cursor-pointer rounded-full bg-gray-900 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-[#1976D2]">
-                <input
-                  type="checkbox"
-                  id="AcceptConditions"
-                  class="peer sr-only"
-                />
-                <span class="absolute inset-y-0 start-0 m-1 size-5 rounded-full bg-gray-300 ring-[5px] ring-inset ring-white transition-all peer-checked:start-7 bg-gray-900 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"></span>
-              </label>
+                {mood ? (
+                  <span className="flex gap-2 text-4xl text-white">
+                    <GiMoonOrbit />
+                  </span>
+                ) : (
+                  <span className="flex gap-2 text-4xl text-yellow-400">
+                    <GiUbisoftSun />
+                  </span>
+                )}
+              </button>
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import mainimg from "../../public/mainimg.png";
 import location from "../../public/location.svg";
 import { useTranslation } from "react-i18next";
+import { createContext } from "react";
+export const MoodeContext = createContext()
 function Home() {
+
+
+
+const [mood, setMood] = useState(() => {
+    return localStorage.getItem("darkmode") === "true";
+  });
+
+useEffect(()=>{
+localStorage.setItem('darkmode', mood)
+},[mood])
+
+
   const { t, i18n } = useTranslation();
   return (
-    <div>
-      <Header />
-      <div className="flex justify-center pt-6 ">
-        <div className="flex bg-[#E4F7FF] w-[1388.33px] h-[560px] drop-shadow-xl  rounded-[50px]  items-center justify-around">
-          <div className="flex flex-col w-[500px] gap-[23px]">
-            <p className="font-sans font-normal">{t("firstext")}</p>
-            <div className="">
-              <h2 className="font-extrabold text-4xl text-[#00085E]">
-                {t("keep")}
-              </h2>
-            </div>
-            <div className="flex gap-5 ">
-              <img src={location} alt="" />
-              <p className="text-1xl font-sans font-bold">{t("desc")}</p>
-            </div>
-            <h2 className="font-bold text-3xl font-sans">{t("our")}</h2>
-            <p className="font-sans text-lg text-[#00085E]">{t("lasttext")}</p>
-            <b className="p-[5px]  rounded-[30px] bg-white flex justify-between shadow-md">
-              <input
-                type="text"
-                placeholder="Your email address"
-                className="outline-none p-3 rounded-[30px]"
-              />
-              <button className="   pt-3 pb-3 border rounded-3xl pl-6 pr-6 text-white font-sans  font-medium bg-blue-500">
-                JOIN LIST!
-              </button>
-            </b>
-          </div>
-          <div className="flex items-center">
-            <img className="w-[450px]" src={mainimg} alt="" />
-          </div>
-        </div>
+
+ <MoodeContext.Provider value={{mood , setMood}}>
+       <div className={mood ? "dark": ""}>
+
+<Header />
+<div className="flex justify-center pt-6 dark:bg-black ">
+  <div className="flex mb-16 bg-[#E4F7FF] w-[1388.33px] h-[560px] drop-shadow-xl  rounded-[50px]  items-center justify-around dark:bg-[#161616]">
+    <div className="flex flex-col w-[500px] gap-[23px]">
+      <p className="font-sans font-normal dark:text-[#999999]">
+        {t("firstext")}
+      </p>
+      <div className="">
+        <h2 className="font-extrabold text-4xl text-[#00085E] dark:text-[#A1A9FF]">
+          {t("keep")}
+        </h2>
       </div>
+      <div className="flex gap-5 ">
+        <img src={location} alt="" />
+        <p className="text-1xl font-sans font-bold dark:text-[#878787]">
+          {t("desc")}
+        </p>
+      </div>
+      <h2 className="font-bold text-3xl font-sans dark:text-white">
+        {t("our")}
+      </h2>
+      <p className="font-sans text-lg text-[#00085E] dark:[#999999] dark:text-[#999999]">
+        {t("lasttext")}
+      </p>
+      <b className="p-[5px]  rounded-[30px] bg-white flex justify-between shadow-md dark:bg-[#0D0D0D]">
+        <input
+          type="text"
+          placeholder="Your email address"
+          className="outline-none p-3 rounded-[30px] dark:bg-[#0D0D0D] dark:text-white"
+        />
+        <button className="   pt-3 pb-3 border rounded-3xl pl-6 pr-6 text-white font-sans  font-medium bg-blue-500">
+          JOIN LIST!
+        </button>
+      </b>
     </div>
+    <div className="flex items-center">
+      <img className="w-[450px]" src={mainimg} alt="" />
+    </div>
+  </div>
+</div>
+</div>
+ </MoodeContext.Provider>
   );
 }
 
